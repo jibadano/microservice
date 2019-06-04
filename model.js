ed6368327877631044362dd1d6fe180f762730d2
Juan Badano <jb@target-solutions.net>
2019-06-04 16:23:34 +0200

    model fix

--- model.js
+++ model.js
@@ -5,13 +5,12 @@ module.exports = class Model {
     constructor(config) {
         mongoose.connect(config.get('mongo'));
         const modelDir = config.get('model.path')
-        this.model = {}
         fs.readdirSync(modelDir).forEach(schemaFile => {
             if (schemaFile !== 'index.js') {
                 const schema = require(path.resolve(`${modelDir}/${schemaFile}`))
                 let schemaName = schemaFile.replace('.js', '')
                 schemaName = schemaName.charAt(0).toUpperCase() + schemaName.slice(1)
-                this.model[schemaName] = mongoose.model(schemaName, schema)
+                this[schemaName] = mongoose.model(schemaName, schema)
             }
         })
     }
