6c1fc6b2a22c116469d127d00907a96878798f34
Juan Badano <jb@target-solutions.net>
2019-08-15 17:53:47 +0200

    fix for path

--- model.js
+++ model.js
@@ -5,13 +5,13 @@ const path = require('path')
 module.exports = class Model {
   constructor(config) {
     console.info(`🌎 Model init`)
-    const path = config.get('model.path') || 'src/model'
+    const modelPath = config.get('model.path') || 'src/model'
     mongoose
       .connect(config.get('mongo'), { useNewUrlParser: true })
       .then(() => console.info(`🌎 Model db connected`))
       .catch(console.error)
 
-    const modelDir = process.env.PWD + '/' + path
+    const modelDir = process.env.PWD + '/' + modelPath
     console.info(`🌎 Model reading from ${modelDir}`)
     fs.readdirSync(modelDir).forEach(schemaFile => {
       if (schemaFile !== 'index.js') {
