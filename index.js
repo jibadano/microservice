83aac07f430028bc47df37657494c666cf343734
Juan Badano <jb@target-solutions.net>
2020-04-09 19:32:54 +0200

    jwt

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const jwt = require('express-jwt')
+const get = require('lodash/get')
 const jsonwebtoken = require('jsonwebtoken')
 
 const { ApolloServer } = require('apollo-server-express')
@@ -60,7 +60,21 @@ module.exports = class Microservice {
       this.sign = (data) =>
         jsonwebtoken.sign(data, jwtOptions.secret, jwtSignOptions)
 
-      app.use(jwt(jwtOptions))
+      app.use((req, res, next) => {
+        const auth = get(req, 'headers.authorization')
+        if (auth) {
+          try {
+            req.user = jsonwebtoken.verify(
+              auth.replace('Bearer ', ''),
+              jwtOptions.secret,
+              jwtSignOptions
+            )
+          } catch (e) {
+            //session expired
+          }
+        }
+        next()
+      })
     }
 
     // Tracing
