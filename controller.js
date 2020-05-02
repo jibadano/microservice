bde139baee9890b3b256a861e28666d1d3393f18
Juan Badano <jb@target-solutions.net>
2020-05-02 18:01:23 +0200

    Auth by role

--- controller.js
+++ controller.js
@@ -4,15 +4,49 @@ const path = require('path')
 const { SchemaDirectiveVisitor } = require('apollo-server-express')
 
 class AuthDirective extends SchemaDirectiveVisitor {
-  visitFieldDefinition(field) {
-    const { resolve } = field
+  visitObject(type) {
+    this.ensureFieldsWrapped(type)
+    type._requiredAuthRole = this.args.requires
+  }
+  // Visitor methods for nested types like fields and arguments
+  // also receive a details object that provides information about
+  // the parent and grandparent types.
+  visitFieldDefinition(field, details) {
+    this.ensureFieldsWrapped(details.objectType)
+    field._requiredAuthRole = this.args.requires
+  }
 
-    field.resolve = (...args) => {
-      const [, , context] = args
-      if (!context.session || !context.session.user)
-        throw new AuthenticationError('Session required')
-      else return resolve.apply(this, args)
-    }
+  ensureFieldsWrapped(objectType) {
+    // Mark the GraphQLObjectType object to avoid re-wrapping:
+    if (objectType._authFieldsWrapped) return
+    objectType._authFieldsWrapped = true
+
+    const fields = objectType.getFields()
+
+    Object.keys(fields).forEach((fieldName) => {
+      const field = fields[fieldName]
+      const { resolve = defaultFieldResolver } = field
+      field.resolve = async function (...args) {
+        // Get the required Role from the field first, falling back
+        // to the objectType if no Role is required by the field:
+        const requiredRole =
+          field._requiredAuthRole || objectType._requiredAuthRole
+
+        if (!requiredRole) {
+          return resolve.apply(this, args)
+        }
+
+        const context = args[2]
+        if (
+          !context.session ||
+          !context.session.user ||
+          ![requiredRole, 'ADMIN'].includes(context.session.user.role)
+        )
+          throw new AuthenticationError('Session required')
+
+        return resolve.apply(this, args)
+      }
+    })
   }
 }
 
@@ -22,7 +56,7 @@ module.exports = class Controller {
     const servicesPath = config.get('services.path') || 'src/services'
     this.typeDefs = [
       gql`
-        directive @auth on FIELD_DEFINITION
+        directive @auth(requires: Role = USER) on OBJECT | FIELD_DEFINITION
 
         type Query {
           version: String
@@ -30,6 +64,12 @@ module.exports = class Controller {
 
         type Mutation {
           refreshConfig: Boolean
+        }
+
+        enum Role {
+          GUEST
+          USER
+          ADMIN
         }
       `
     ]
